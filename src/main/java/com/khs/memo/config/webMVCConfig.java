48d45a971b13cfddf70c7f1549b7740febf5d3cc
DESKTOP-NJKHAHU\user <mile@naver.com>
2022-08-23 20:49:42 +0900

    [김효석]  - 로그인 했을때 회원가입 페이지 못들어가게 하기  - 마무리

--- src/main/java/com/khs/memo/config/webMVCConfig.java
+++ src/main/java/com/khs/memo/config/webMVCConfig.java
@@ -1,6 +1,8 @@
 package com.khs.memo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +11,9 @@ import com.khs.memo.common.FileManagerService;
 @Configuration
 public class webMVCConfig implements WebMvcConfigurer{
 
+	@Autowired
+	private PermissionInterceptor interceptor;
+	
 	// 특정 경로의 파일을 외부에서 접근 가능하도록
 	// 그 파일의 접근 경로를 잡아준다.
 	
@@ -17,5 +22,16 @@ public class webMVCConfig implements WebMvcConfigurer{
 		
 		registry.addResourceHandler("/images/**")
 		.addResourceLocations("file:///" + FileManagerService.FILE_UPLOAD_PATH);
+	}
+	
+	// 인터셉터 등록
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {{
+		
+		registry.addInterceptor(interceptor)
+		.addPathPatterns("/**") // 어떤 경로(path)가 인터셉터를 거쳐서 수행될지 설정
+		.excludePathPatterns("/static/**", "/images/**", "/user/signout"); 
+		
+	 }
 	}
 }
